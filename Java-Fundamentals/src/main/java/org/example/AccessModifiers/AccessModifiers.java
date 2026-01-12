74611061fdb3f12e3cec8ac04f12d8d42e8fd639
Vishal Kumar Maurya <118094180+vmaurya6622@users.noreply.github.com>
2026-01-12 21:28:24 +0530

    update to access modifiers

--- Java-Fundamentals/src/main/java/org/example/AccessModifiers/AccessModifiers.java
+++ Java-Fundamentals/src/main/java/org/example/AccessModifiers/AccessModifiers.java
@@ -1,4 +1,21 @@
 package org.example.AccessModifiers;
-
+/*
+Access modifiers are used to control the visibility and accessibility of classes, variables, methods and constructors.
+JAVA has its 4 types:
+  -> Public:
+        - Accessible anywhere.
+        - Used for Api's and methods meant to be used for other classes.
+  -> Private:
+        - Accessible within the same class.
+        - protects internal data and integrity.
+  -> Protected:
+        - Accessible within same package and in subclasses.
+        - primarily used when inheritance is involved.
+  -> default:
+        - when no keyword is used. accessible only in same package.
+        - mainly used for internal package-level logics.
+ */
 public class AccessModifiers {
+    // private
+    
 }
--- Java-Fundamentals/src/main/java/org/example/streams/StreamsApiDemo.java
+++ Java-Fundamentals/src/main/java/org/example/streams/StreamsApiDemo.java
@@ -1,4 +1,5 @@
 package org.example.streams;
 
+
 public class StreamsApiDemo {
 }
