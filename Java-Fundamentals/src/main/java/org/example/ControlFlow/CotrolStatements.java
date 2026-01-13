baff072176e2c39b55361b3930960f286b6fbb5a
Vishal Kumar Maurya <118094180+vmaurya6622@users.noreply.github.com>
2026-01-13 16:43:16 +0530

    Control Flow Done

--- Java-Fundamentals/src/main/java/org/example/ControlFlow/CotrolStatements.java
+++ Java-Fundamentals/src/main/java/org/example/ControlFlow/CotrolStatements.java
@@ -19,25 +19,86 @@ package org.example.ControlFlow;
             - return
  */
 public class CotrolStatements {
-    public static void main(String[] args)
-    {
+    public static void main(String[] args) {
         //IF and ELSE
-        int age=15;
-        if(age<18){
+        int age = 15;
+        if (age < 18) {
             System.out.println("You are too young to drive and vote");
 
-        }else{
+        } else {
             System.out.println("You can drive as well as vote!");
         }
         //IF-ELSE-IF
-        int score=41;
-        if(score<33){
+        int score = 41;
+        if (score < 33) {
             System.out.println("You Failed");
-        } else if (score>33 && score <70) {
+        } else if (score > 33 && score < 70) {
             System.out.println("You are average");
-        }else{
+        } else {
             System.out.println("you are a good student.");
         }
-    }
+        //switch statement
+        int day = 3;
+        switch (day) {
+            case 1:
+                System.out.println("monday");
+                break;
+            case 2:
+                System.out.println("tuesday");
+                break;
+            case 3:
+                System.out.println("wednesday");
+                break;
+            case 4:
+                System.out.println("thursday");
+                break;
+            case 5:
+                System.out.println("friday");
+                break;
+            case 6:
+                System.out.println("saturday");
+                break;
+            case 7:
+                System.out.println("sunday");
+                break;
+            default:
+                System.out.println("Invalid day");
+        }
+        //FOR Loop
+        for (int i = 0; i <= 5; i++) {
+            System.out.println("for loop i = " + i);
+        }
+        //while loop
+        int i = 4;
+        while (i == 0) {
+            System.out.println("while loop i = " + i);
+            i--;
+        }
+        // Do-While Loop
+        int num = 1;
+        do {
+            System.out.println("Do-while is executing...\n ");
+            num++;
+        } while (num < 5);
+        // Enhanced For-loop (FOR-EACH)
+        int[] arr = {10, 11, 12, 13, 14, 15};
+        for (int value : arr) {
+            System.out.println("For-Each running value : " + value);
+        }
 
+        // Break Statement
+        for (int ii = 1; ii <= 5; ii++) {
+            if (ii == 3) {
+                break;
+                //System.out.println("this will not be printed");
+            }
+            System.out.println("Break check running value : " + ii);
+        }
+        //similarly "continue" statements doesn't hold the code they just allow the code to run.
+        // Return Statements
+        System.out.println("Square of 4 is? : " + square(4));
+    }
+    static int square(int o) {
+        return o * o;
+    }
 }
