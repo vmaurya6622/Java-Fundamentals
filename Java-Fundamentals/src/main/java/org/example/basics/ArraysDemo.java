fa39efb3f110c07630e1461fa945ca13ff856466
Vishal Kumar Maurya <118094180+vmaurya6622@users.noreply.github.com>
2026-01-13 10:46:20 +0530

    Basics Done

--- Java-Fundamentals/src/main/java/org/example/basics/ArraysDemo.java
+++ Java-Fundamentals/src/main/java/org/example/basics/ArraysDemo.java
@@ -1,4 +1,43 @@
 package org.example.basics;
 
+import java.util.Arrays;
+
+/*
+     -> Array is an object used to store multiple values of same datatype in a contiguous memory structure.
+     Key Points:
+     -> fixed size; indexing starts from 0; dafault values automatically assigned;
+     -> faster access to memory using indices.
+ */
 public class ArraysDemo {
+    static void changeArray(int[]arr){
+        for(int i=0;i< arr.length;i++){
+            arr[i]=arr[i]*2; // i am incrementing every arr element by twice.
+        }
+    }
+
+    static void main() {
+        int [] num={14,85,11,10,21};
+        System.out.println("Original Array: ");
+        System.out.println(Arrays.toString(num));
+        System.out.println();
+
+        System.out.println("Accessing Elements:");
+        System.out.println("First Element: "+ num[0]);
+        System.out.println("Last Element: "+num[num.length-1]);
+
+        System.out.println("Print all elements: ");
+        for(int i=0;i<num.length;i++) System.out.println("Element @ idx "+i+" is: "+num[i]);
+
+        System.out.println("\nNow Sorting: ");
+        Arrays.sort(num);
+        for(int i=0;i<num.length;i++) System.out.println("After Sorting Element @ idx "+i+" is: "+num[i]);
+
+        System.out.println("Call by value V/s Call by reference: ");
+        System.out.println();
+
+        System.out.println("Before changeArray() call: "+Arrays.toString(num));
+        changeArray(num);
+        System.out.println("After changeArray() call: "+Arrays.toString(num));
+
+    }
 }
--- Java-Fundamentals/src/main/java/org/example/basics/StringsDemo.java
+++ Java-Fundamentals/src/main/java/org/example/basics/StringsDemo.java
@@ -7,14 +7,58 @@ String v/s StringBuilder
                    or, String s = "Shreya";
 
     In java.lang.string there are 3 types of classes which implements serializable, comparable and charsequence.
-   So, in java we can create strings using StringBuilder, StringBuffer and String classes.
-   String Class is immutable while others i.e. StringBuilder and StringBuffer are mutable.
+    classes which implement CharSequence are String, StringBuilder, StringBuffer.
+    So, in java we can create strings using StringBuilder, StringBuffer and String classes.
+    String Class is immutable i.e. any modification creates new objects while others i.e. StringBuilder(Faster + Thread Unsafe + Single Threaded)
+    and StringBuffer(Thread safe + Multithreaded + slower) are mutable.
 
- -> Now, lets talk about string pools which is a specific region/ area in the heap memory. it stores
+ -> Now, lets talk about string pools which is a specific region/ area in the heap memory. JVM stores only one copy
+    of each unique literal. If same literal is reused, JVM returns the same reference. it improves memory and performance.
+
 
  */
 public class StringsDemo {
+
     static void main() {
-        System.out.println("hel");
+        //Initialisation using literals
+        String s1="Hello";
+        String s2="Hello";
+        System.out.println("s1 == s2?? : "+(s1==s2)); // true because both refer to the same object in spring pool.
+        String s3=new String("Hello");
+        String s4=new String("Hello");
+        System.out.println("s3 == s4?? : "+(s3==s4)); // false because both are different objects in heap.
+        System.out.println("\nValue Comparison: ");
+        System.out.println("s1.equals(s2)?? : "+(s1.equals(s2)));
+        System.out.println("s3.equals(s4)?? : "+(s3.equals(s4)));
+
+        System.out.println("\nCheck Immutability!");
+        String original="java";
+        original.concat("is object oriented."); // this creates a new object that's why it is ignored.
+        System.out.println("After concat without reference: "+original);
+        original=original.concat("but not sure!!"); //via assignment operators
+        System.out.println("After concat with reference: "+original);
+
+        System.out.println("Java String Fundamentals: ");
+
+        String test="Ciena is a CN based company.";
+        System.out.println("Length: "+test.length());
+        System.out.println("Trimmed: "+test.trim());
+        System.out.println("UpperCase: "+test.toUpperCase());
+        System.out.println("LowerCase: "+test.toLowerCase());
+        System.out.println("Starts with 'A'?: "+test.startsWith("A"));// false as it starts with 'C'
+        System.out.println("char at idx 3: "+test.charAt(3));
+
+        System.out.println("StringBuilder!!");
+        StringBuilder sb  =  new StringBuilder("Hello");
+        sb.append("world");
+        sb.append("!");
+        System.out.println("StringBuilder Result: "+sb);
+
+        System.out.println("ThreadSafe StringBuffer");
+        StringBuffer sbf=new StringBuffer("Helloo");
+        sbf.append("meee");
+        System.out.println("StringBuffer Result: "+sbf);
+        // Initialisation using new keyword
+//        System.out.println("hel");
     }
 }
