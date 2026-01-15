33446f7d2cee83a391f8fb6b25af47dade48b306
Vishal Kumar Maurya <118094180+vmaurya6622@users.noreply.github.com>
2026-01-15 16:19:18 +0530

    Executors done

---
+++ Java-Fundamentals/src/main/java/org/example/multithreading/Executorz.java
@@ -0,0 +1,44 @@
+package org.example.multithreading;
+
+import java.util.concurrent.ExecutorService;
+
+/*
+    -> Executors Framework is a high level API for managing threads and running tasks without manually creating
+       and managing any thread objects.
+    -> It has task submission through Runnable/ Callable and Task execution(using Thread pool).
+    -> Runnable:
+            - It represent a task that does not return any results. It also cannot check exceptions and has single method which is void run();
+    -> Callable:
+            - Callable represents a task that returns a result which is a generic result. it can throw checked exceptions.
+              it also has only a single method that is v call() throws exception.
+    -> Future:
+            - It is an interface that represents the result of an asynchronized computation.
+            - After submitting callable task, Future object is returned.
+            - acts as a placeholder for the results.
+ */
+import java.util.concurrent.*;
+public class Executorz {
+    static void main(String[] args) throws ExecutionException, InterruptedException {
+        ExecutorService pool = Executors.newFixedThreadPool(4);
+        Callable<String> task = new Callable<String>() {
+            @Override
+            public String call() throws Exception {
+                Thread.sleep(900); //sleeping thread by 900ms.
+                return "Hello World";
+            }
+        };
+        Future<String> hiii = pool.submit(task);
+        System.out.println("Result is: "+hiii.get());
+        pool.shutdown();
+
+        //scheduling threads
+        ScheduledExecutorService schedules = Executors.newScheduledThreadPool(2);
+        schedules.scheduleAtFixedRate(()->{
+            System.out.println("Task is being executed every 10 seconds.");
+        },1,10,TimeUnit.SECONDS); // delay of 1 seconds and runs every 10 seconds
+
+        schedules.schedule(()->{
+            System.out.println("Task is being executed every 10 seconds.");
+        },10,TimeUnit.SECONDS); // executes after a delay of 10 seconds
+    }
+}
--- Java-Fundamentals/src/main/java/org/example/multithreading/ThreadPool.java
+++ Java-Fundamentals/src/main/java/org/example/multithreading/ThreadPool.java
@@ -1,4 +1,9 @@
 package org.example.multithreading;
+
+import java.lang.reflect.Executable;
+import java.util.concurrent.Executors;
+import java.util.concurrent.ExecutorService;
+import java.util.concurrent.Executors;
 /*
     -> It is a mechanism to manage a group of reusable thread for executing a set of tasks.
     -> it helps to prevent creating thread for every task.
@@ -6,6 +11,17 @@ package org.example.multithreading;
  */
 public class ThreadPool {
     static void main() {
-        
+        ExecutorService pool = Executors.newFixedThreadPool(6);
+        for(int i = 0; i <= 10; i++) {
+            int taskID=i;
+            // submit a task
+            pool.execute(new Runnable(){
+               @Override
+               public void run() {
+                   System.out.println("Task "+taskID+" is executed by "+Thread.currentThread().getName());
+               }
+            });
+        }
+        pool.shutdown();
     }
 }
