de2d9269613219be1b3ac2aa5df1c542a10d274b
Vishal Kumar Maurya <118094180+vmaurya6622@users.noreply.github.com>
2026-01-15 14:52:03 +0530

    ThreadPool

---
+++ Java-Fundamentals/src/main/java/org/example/multithreading/ThreadPool.java
@@ -0,0 +1,11 @@
+package org.example.multithreading;
+/*
+    -> It is a mechanism to manage a group of reusable thread for executing a set of tasks.
+    -> it helps to prevent creating thread for every task.
+    -> hence, when a new task is assigned, thread pool is assigned to the available thread.
+ */
+public class ThreadPool {
+    static void main() {
+        
+    }
+}
