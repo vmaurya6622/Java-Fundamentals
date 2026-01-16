3291290493bca2a78e39757940cb6ca607ae7e3e
Vishal Kumar Maurya <118094180+vmaurya6622@users.noreply.github.com>
2026-01-16 10:05:37 +0530

    ConsumerSuppliers Done

--- Java-Fundamentals/src/main/java/org/example/streamsAPI/ConsumerSupplier.java
+++ Java-Fundamentals/src/main/java/org/example/streamsAPI/ConsumerSupplier.java
@@ -1,11 +1,24 @@
 package org.example.streamsAPI;
-
+/*
+    It showcases four core Java functional interfaces—Consumer, Supplier, Predicate, and Functions.
+ */
 import java.util.function.Consumer;
+import java.util.function.Function;
+import java.util.function.Predicate;
+import java.util.function.Supplier;
 
 public class ConsumerSupplier {
     static void main() {
         Consumer<Integer> consumer = x -> System.out.println(x);
         consumer.accept(11);
-        
+
+        Supplier<String > supplier = () -> "Hello"; //this functional interface only gives and does not takes.
+        System.out.println(supplier.get());
+
+        Predicate<Integer> isOdd= x -> x%2==1;
+        Function<Integer,Integer> function = x -> x*5;
+        Consumer<Integer> consumer1 = x -> System.out.println(x);
+        Supplier<Integer>supplier1=()->100;
+        System.out.println(supplier1.get());
     }
 }
